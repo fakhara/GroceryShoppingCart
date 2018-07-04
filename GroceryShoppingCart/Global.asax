4fca6c11bfa3ddacc07e979b8be21e4465231d05
deltagare <deltagare@LEXJONKAF19>
2018-07-04 08:19:18 +0200

    Add project files.

---
+++ GroceryShoppingCart/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GroceryShoppingCart.MvcApplication" Language="C#" %>
---
+++ GroceryShoppingCart/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GroceryShoppingCart.Startup))]
+namespace GroceryShoppingCart
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
